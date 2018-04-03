b1f760d9d57c4ad9ad55089d38b43d4199292ae4
Michael Myers <myersmyer@gmail.com>
2018-04-03 18:25:55 -0400

    added button function

--- assets/script.js
+++ assets/script.js
@@ -3,8 +3,24 @@ $(document).ready(function () {
 
 var topics = ['rottweiler', 'drums', 'album covers','dessert', 'video games' ];
 
+console.log('my js file is being read');
 
 
 
+function renderButtons () {
+    console.log('my render button function is being read');
+    $('#topicButtons').empty();
+
+    for (let i = 0; i < topics.length; i++) {
+        var a = $('<button>');
+        console.log(a);
+        a.addClass('topic-btn')
+        a.attr('data-name', topics[i]);
+        a.text(topics[i]);
+        $('#topicButtons').append(a);
+    }
+}
+
+renderButtons();
 
 });
