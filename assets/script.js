7b8db63ac3990a2b56fc394ee8196108c2be63bc
Michael Myers <myersmyer@gmail.com>
2018-04-04 22:56:14 -0400

    basic styling and first gif and rating are populating page

--- assets/script.js
+++ assets/script.js
@@ -1,41 +1,58 @@
 $(document).ready(function () {
 
 
-var topics = ['rottweiler', 'drums', 'album covers','dessert', 'video games' ];
-// My api key =  T8BtFJivaeZt7qHxp2Qw17qsPXxyovkg
+    var topics = ['rottweiler', 'drums', 'album covers', 'dessert', 'video games', 'pizza'];
+    // My api key =  T8BtFJivaeZt7qHxp2Qw17qsPXxyovkg
 
-console.log('my js file is being read');
+    console.log('my js file is being read');
 
-function displayGiphy (){
-    var addTopic = $(this).attr('data-name');
-    var queryURL = 'https://api.giphy.com/v1/gifs/search?q=' + addTopic + '&api_key=T8BtFJivaeZt7qHxp2Qw17qsPXxyovkg&limit=10';
+    function displayGiphy() {
+        var addTopic = $(this).attr('data-name');
+        var queryURL = 'https://api.giphy.com/v1/gifs/search?q=' + addTopic + '&api_key=T8BtFJivaeZt7qHxp2Qw17qsPXxyovkg&limit=10';
 
-    $.ajax({
-        url: queryURL,
-        method: 'GET'
-    }).then(function(response) {
-       console.log(response);
-    });
-}
+        $.ajax({
+            url: queryURL,
+            method: 'GET'
+        }).then(function (response) {
+            console.log(response);
+            // create empty div for new gif / ratings to populate
+            var topicDiv = $("<div class='topic'>");
+            // create image element and append to empty div
+            var imgURL = response.data[0].images.fixed_width_still.url;
+            var image = $('<img>').attr('src', imgURL);
+            topicDiv.append(image);
+            // create rating element and append to empty div
+            var rating = response.data[0].rating;
+            var pOne = $('<p>').text('this gif is rated: ' + rating);
+            topicDiv.append(pOne);
 
+            // prepend results to page element
+            $('#gifDump').prepend(topicDiv);
+        });
 
-function renderButtons () {
-    console.log('my render button function is being read');
-    $('#topicButtons').empty();
+        //for the image use response.0.images.fixed_width_still.url
+        // for the rating use response.data[].rating
 
-    for (let i = 0; i < topics.length; i++) {
-        var a = $('<button>');
-        console.log(a);
-        a.addClass('topic-btn')
-        a.attr('data-name', topics[i]);
-        a.text(topics[i]);
-        $('#topicButtons').append(a);
     }
-}
+
+
+    function renderButtons() {
+        console.log('my render button function is being read');
+        $('#topicButtons').empty();
+
+        for (let i = 0; i < topics.length; i++) {
+            var a = $('<button>');
+            console.log(a);
+            a.addClass('topic-btn')
+            a.attr('data-name', topics[i]);
+            a.text(topics[i]);
+            $('#topicButtons').append(a);
+        }
+    }
 
 
     $(document).on('click', '.topic-btn', displayGiphy);
 
-renderButtons();
+    renderButtons();
 
 });
