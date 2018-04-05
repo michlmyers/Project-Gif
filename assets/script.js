758298f114a82a3d754e2343ebf4860a07cbc65b
Michael Myers <myersmyer@gmail.com>
2018-04-04 21:15:50 -0400

    buttons and api call are working

--- assets/script.js
+++ assets/script.js
@@ -2,9 +2,21 @@ $(document).ready(function () {
 
 
 var topics = ['rottweiler', 'drums', 'album covers','dessert', 'video games' ];
+// My api key =  T8BtFJivaeZt7qHxp2Qw17qsPXxyovkg
 
 console.log('my js file is being read');
 
+function displayGiphy (){
+    var addTopic = $(this).attr('data-name');
+    var queryURL = 'https://api.giphy.com/v1/gifs/search?q=' + addTopic + '&api_key=T8BtFJivaeZt7qHxp2Qw17qsPXxyovkg&limit=10';
+
+    $.ajax({
+        url: queryURL,
+        method: 'GET'
+    }).then(function(response) {
+       console.log(response);
+    });
+}
 
 
 function renderButtons () {
@@ -20,6 +32,9 @@ function renderButtons () {
         $('#topicButtons').append(a);
     }
 }
+
+
+    $(document).on('click', '.topic-btn', displayGiphy);
 
 renderButtons();
 
